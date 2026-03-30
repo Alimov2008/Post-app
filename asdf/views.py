e28ed699497f805e8c55f2e64a4ff8bf895e5dc1
Ali <alimovmuhammadali01222008@gmail.com>
2026-03-30 18:05:03 +0500

    added post view

--- asdf/views.py
+++ asdf/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from asdf.models import Post
+
 
 def home(request):
     return render(request, "asdf/home.html")
@@ -8,3 +10,8 @@ def home(request):
 
 def about(request):
     return render(request, "asdf/about.html")
+
+
+def posts(request):
+    posts = Post.objects.all()
+    return render(request, "asdf/posts.html", {"posts": posts})
