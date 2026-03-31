5c582fa017cd43483c9d148e06970d6e41b4e5bd
Ali <alimovmuhammadali01222008@gmail.com>
2026-03-31 16:31:55 +0500

    make login required

--- asdf/views.py
+++ asdf/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 
 from asdf.models import Post
@@ -9,20 +10,24 @@ def home(request):
     return render(request, "asdf/home.html")
 
 
+@login_required
 def about(request):
     return render(request, "asdf/about.html")
 
 
+@login_required
 def posts(request):
     posts = Post.objects.all()
     return render(request, "asdf/posts.html", {"posts": posts})
 
 
+@login_required
 def post_details(request, id):
     post = get_object_or_404(Post, id=id)
     return render(request, "asdf/post_details.html", {"post": post})
 
 
+@login_required
 def create_post(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -34,6 +39,7 @@ def create_post(request):
     return render(request, "asdf/create_post.html", {"form": form})
 
 
+@login_required
 def update_post(request, id):
     post = get_object_or_404(Post, id=id)
     if request.method == "POST":
@@ -46,6 +52,7 @@ def update_post(request, id):
     return render(request, "asdf/update_post.html", {"form": form})
 
 
+@login_required
 def delete_post(request, id):
     post = get_object_or_404(Post, id=id)
     if request.method == "POST":
